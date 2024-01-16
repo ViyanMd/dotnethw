8ff33a6a959bd0b08cea32010b03d627cf2c2b1d
viyan <viyanomd@gmail.com>
2024-01-16 15:46:57 +0100

    initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            btInitial = new Button();
+            lbMainFormClose = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Variable Display Semib", 11.7818184F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(265, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(242, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Let's get to know each other";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // btInitial
+            // 
+            btInitial.Anchor = AnchorStyles.None;
+            btInitial.Cursor = Cursors.Hand;
+            btInitial.FlatStyle = FlatStyle.System;
+            btInitial.Font = new Font("Segoe UI Historic", 9.818182F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btInitial.Location = new Point(319, 172);
+            btInitial.Name = "btInitial";
+            btInitial.Size = new Size(140, 49);
+            btInitial.TabIndex = 1;
+            btInitial.Text = "Let's go!";
+            btInitial.UseVisualStyleBackColor = true;
+            btInitial.Click += btInitial_Click;
+            // 
+            // lbMainFormClose
+            // 
+            lbMainFormClose.Anchor = AnchorStyles.None;
+            lbMainFormClose.AutoSize = true;
+            lbMainFormClose.Cursor = Cursors.Hand;
+            lbMainFormClose.Font = new Font("Segoe UI Historic", 9.163636F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbMainFormClose.ForeColor = SystemColors.HotTrack;
+            lbMainFormClose.Location = new Point(305, 224);
+            lbMainFormClose.Name = "lbMainFormClose";
+            lbMainFormClose.Size = new Size(173, 19);
+            lbMainFormClose.TabIndex = 2;
+            lbMainFormClose.Text = "I dont wanna do that :(";
+            lbMainFormClose.Click += lbMainFormClose_Click;
             // 
             // FormMain
             // 
-            AutoScaleDimensions = new SizeF(8F, 19F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(800, 450);
+            Controls.Add(lbMainFormClose);
+            Controls.Add(btInitial);
+            Controls.Add(label1);
+            MinimumSize = new Size(818, 494);
             Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Application";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button btInitial;
+        private Label lbMainFormClose;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,49 @@ namespace WinFormsApp
         {
             InitializeComponent();
         }
+
+        private void btInitial_Click(object sender, EventArgs e)
+        {
+            var count = 1;
+            var charCount = 0;
+            const string textName = "Name:";
+            const string captionName = "My name is Viyan.";
+            charCount += captionName.Length;
+            var resultName = MessageBox.Show(captionName, textName, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (resultName == DialogResult.OK)
+            {
+                count++;
+                const string textEducation = "Education:";
+                const string captionEducation = "I have a Bachelor's degree in International Business.";
+                charCount += captionEducation.Length;
+                var resultEducation = MessageBox.Show(captionEducation, textEducation, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (resultEducation == DialogResult.OK)
+                {
+                    count++;
+                    const string textOrigin = "Origin:";
+                    const string captionOrigin = "I come from Ukraine.";
+                    charCount += captionOrigin.Length;
+                    var resultOrigin = MessageBox.Show(captionOrigin, textOrigin, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    if (resultOrigin == DialogResult.OK)
+                    {
+                        count++;
+                        const string textOccupation = "Occupation:";
+                        const string captionOccupation = "And I am a software engineering student.";
+                        charCount += captionOccupation.Length;
+                        var resultOccupation = MessageBox.Show(captionOccupation, textOccupation, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+
+            const string textStat = "Statistics: ";
+            string captionStat = "You've visited: " + count + " MessageBoxes";
+            string charStat = "You've read: " + charCount + " letters";
+            MessageBox.Show(captionStat + '\n' + charStat, textStat, MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void lbMainFormClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
